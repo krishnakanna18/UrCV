d0721f04013997638464a8ccc8359b9f868e9428
krishnakanna18 <krishnakanna562@gmail.com>
2021-06-26 20:27:20 +0530

    Added client endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 exports.serverEndPoint="https://urcv.herokuapp.com"
 // exports.serverEndPoint="http://localhost:9000"
-exports.clientEndPoint="https://urcv.netlify.app/"
+exports.clientEndPoint="https://urcv.netlify.app"
 // exports.clientEndPoint="https://jolly-goldberg-0e3408.netlify.app"
