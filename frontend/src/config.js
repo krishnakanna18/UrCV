442a783234030a7468067a3a225299772abeaf1d
krishnakanna18 <krishnakanna562@gmail.com>
2023-06-29 13:00:28 +0530

    Change server endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-exports.serverEndPoint="https://urcv.herokuapp.com"
+exports.serverEndPoint="https://urcv.onrender.com"
 // exports.serverEndPoint="http://localhost:9000"
 exports.clientEndPoint="https://urcv.netlify.app"
 // exports.clientEndPoint="http://localhost:3000"
